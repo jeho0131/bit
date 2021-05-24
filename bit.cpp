2a1c89484fd3c0900f97f92e9e1b7844bb875317
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-05-24 19:07:22 +0900

    Add files via upload

---
+++ bit.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+int main() {
+	uint8_t a;
+	int8_t b;
+	uint8_t n;
+	
+	unsigned int num;
+	
+	scanf("%d", &num);
+
+	unsigned int c;
+	int ni;
+	uint8_t nn;
+
+	for(int j = 0; j <= 32; j++) {
+		if (pow(2,j) < num && pow(2, j+1) >= num) {
+			c = pow(2,j+1);
+			ni = j;
+		}
+	}
+	
+	for(int i = 0; i <= ni+1; i++) { 
+		nn = c & num;
+		if (nn > 0) {
+			printf("1");
+		}
+		else {
+			printf("0");
+		}		
+		c = c >> 1;
+	}
+	
+	return 0;
+}
